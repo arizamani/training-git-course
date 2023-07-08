20590845b5a8e8e2f3410ec599606c430c3904df
Arash Zamani <arizamani@gmail.com>
2023-07-08 16:19:37 +0330

    Add custom middlewar

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
+const logger = require("./logger");
 const Joi = require("joi");
 const express = require("express");
 const app = express();
+
+//this is kind of Middlewar
 app.use(express.json());
+
+app.use(logger);
 
 const courses = [
     { id:1 , name: "Course1"},
@@ -36,26 +41,63 @@ app.get("/api/posts/:year/:month", function(req,res){
 
 
 app.post("/api/courses", function(req,res){
-    const schema = Joi.object({
-        name : Joi.string().min(3).required()
-    });
-
-    const result = schema.validate(req.body);
-
-    if(result.error){
-        res.status(400).send(result.error.message);
+    
+    const {error,value} = validateCourse(req.body)
+    if(error){
+        //console.log(result.error)
+        res.status(400).send(error.message);
         return;
     }
     const course ={
         id: courses.length + 1,
-        name: req.body.name
+        name: value.name
     }
     courses.push(course);
-    res.send(courses);
+    res.send(course);
 })
 
+app.put("/api/courses/:id", function(req,res){
+    const course = courses.find(function(a){
+            return a.id === parseInt(req.params.id);
+    })
+    if(!course) {
+            res.status(404).send("The course with given id was not found");
+            return;
+    }
+    
+    const {error,value} = validateCourse(req.body)
+    if(error){
+        //console.log(result.error)
+        res.status(400).send(error.message);
+        return;
+    }
+    course.name = value.name;
+    res.send(course);
+});
+
+app.delete("/api/courses/:id", function(req,res){
+    const course = courses.find(function(a){
+            return a.id === parseInt(req.params.id);
+    })
+    if(!course) {
+            res.status(404).send("The course with given id was not found");
+            return;
+    }
+    
+    const index = courses.indexOf(course);
+    courses.splice(index, 1);
+    res.send(course);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function(){
     console.log(`Listening on port ${port}...`);
 });
+
+function validateCourse(course){
+    const schema = Joi.object({
+        name : Joi.string().min(3).required()
+    });
+
+    return schema.validate(course);
+}
