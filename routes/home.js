29cb710e9fc36debe632e7a6bb3cf6202321ec69
Arash Zamani <arizamani@gmail.com>
2023-07-10 18:59:40 +0330

    Create branch 3-way-method

--- routes/home.js
+++ routes/home.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-
-// This is called a Route
 router.get("/", function(req,res){
     res.render("index",{
         title: "My Express App",
