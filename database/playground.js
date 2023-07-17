e12ea756ad6ca212b546796e2f64fd2147d8ba72
Arash Zamani <arizamani@gmail.com>
2023-07-17 13:16:09 +0330

    Complete mongoose CRUD & Validation

--- database/playground.js
+++ database/playground.js
@@ -14,59 +14,77 @@ db.connect(databaseUrl)
     });
 //Database Schema
 const courseSchema = new db.Schema({
-    name: String,
-    author: String,
+    name: {
+        type: String,
+        required: '{PATH} is required!',
+        /*
+        validate: [
+            {validator: v => v!="" || v!=null, message: "Name field couldn't be empty" },
+            {validator: validator, message: 'uh oh' }
+        ]
+        */
+    },
+    author: {type: String, default: "Arash Zamani"},
     tags: [String],
     date: {type: Date, default: Date.now},
-    isPublished: Boolean
+    isPublished: {type: Boolean, default: false}
 });
 
 const Course = db.model("Course",courseSchema);
 
-async function createCourse(){
+async function getCourses(){
+    const result = await Course.find()
+    .limit(10).
+    sort({name:1});
+    //select({name:1});
+    startupDebug(result);
+    return result;
+} 
+
+async function getCourse(id){
+
+    const course = await Course.findById(id);
+    if (!course) return Promise.reject(new Error("The course with given id was not found",{cause: {code: 404}}));
+
+    startupDebug(course);
+    return course;
+}
+
+async function updateCourse(id,courseObj){
+    
+    const course = await Course.findByIdAndUpdate(id,{$set: courseObj},{new: true});
+    if (!course) return Promise.reject(new Error("The course with given id was not found"));
+
+    startupDebug(course);
+    return course;
+} 
+
+async function createCourse(courseObj){
+    const course = new Course(courseObj);
+    //if (!course) return Promise.reject(new Error("Not valid Name format...",{cause: {code: 400}}));
+    /*
     const course = new Course({
         name: "React Course",
         author: "Arash Zamani",
         tags:["react","frontend"],
         isPublished: true
     });
+    */
     const result = await course.save();
     startupDebug(result);
+    return result;   
 }
 
-async function getCourses(){
-    const result = await Course.find()
-    .limit(10).
-    sort({name:1}).
-    select({name:1});
+async function removeCourse(id){
+    const result = await Course.findByIdAndDelete(id);
+    if (!result) return Promise.reject(new Error("The course with given id was not found",{cause: {code: 404}}));
     startupDebug(result);
     return result;
-} 
-
-async function getCourse(id){
-    if (!id.match(/^[0-9a-fA-F]{24}$/)) return Promise.reject(new Error("Not valid id format..."));
-
-    const course = await Course.findById(id);
-    if (!course) return Promise.reject(new Error("Not found..."));
-
-    startupDebug(course);
-    return course;
 }
 
-async function updateCourse(id){
-    if (!id.match(/^[0-9a-fA-F]{24}$/)) return Promise.reject(new Error("Not valid id format..."));
-    const course = await Course.findById(id);
-    if (!course) return Promise.reject(new Error("Not found..."));
-    course.set({
-        isPublished: true,
-        author: "Another one"
-    });
-    const result = await course.save();
-    startupDebug(result);
-} 
 
-
-module.exports.createCourse= createCourse;
 module.exports.getCourses= getCourses;
 module.exports.getCourse= getCourse;
 module.exports.updateCourse= updateCourse;
+module.exports.createCourse= createCourse;
+module.exports.removeCourse= removeCourse;
--- routes/courses.js
+++ routes/courses.js
@@ -3,13 +3,6 @@ const db = require("../database/playground");
 const Joi = require("joi"); //to validation
 const express = require("express");
 const router = express.Router();
-/*
-const courses = [
-    { id:1 , name: "Course1"},
-    { id:2 , name: "Course2"},
-    { id:3 , name: "Course3"}
-];
-*/
 
 router.get("/", function(req,res){
     db.getCourses().then(function(result){
@@ -19,77 +12,62 @@ router.get("/", function(req,res){
 
 router.get("/:id", function(req,res){
     const id = req.params.id;
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) return res.status(400).send("Not valid id format...");
+
     db.getCourse(id).then(function(result){
         res.send(result);
     }).catch(function(err){
-        res.send(err.message);
+        let errorCode = err.cause.code;
+        res.status(errorCode).send(err.message);
     });
-
-    /*
-    const course = courses.find(function(a){
-        return a.id === parseInt(req.params.id);
-    });
-    if (!course){
-        res.status(404).send("The course with given id was not found");
-        return; 
-    } 
-    res.send(course);
-    */
 });
 
 router.post("/", function(req,res){
-    
-    const {error,value} = validateCourse(req.body)
-    if(error){
-        //console.log(result.error)
-        res.status(400).send(error.message);
-        return;
-    }
-    const course ={
-        id: courses.length + 1,
-        name: value.name
-    }
-    courses.push(course);
-    res.send(course);
+    const {error,value} = validateCourse(req.body,"post");
+    if(error) return res.status(400).send(error.message);
+
+    db.createCourse(value).then(function(result){
+        res.send(result);
+    });
 })
 
 router.put("/:id", function(req,res){
-    const course = courses.find(function(a){
-            return a.id === parseInt(req.params.id);
-    })
-    if(!course) {
-            res.status(404).send("The course with given id was not found");
-            return;
-    }
-    
-    const {error,value} = validateCourse(req.body)
-    if(error){
-        //console.log(result.error)
-        res.status(400).send(error.message);
-        return;
-    }
-    course.name = value.name;
-    res.send(course);
+    const id = req.params.id;
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) return res.status(400).send("Not valid id format...");
+
+    const {error,value} = validateCourse(req.body,"put");
+    if(error) return res.status(400).send(error.message);
+
+    db.updateCourse(id,value).then(function(result){
+        res.send(result);
+    });
+
+
 });
 
 router.delete("/:id", function(req,res){
-    const course = courses.find(function(a){
-            return a.id === parseInt(req.params.id);
-    })
-    if(!course) {
-            res.status(404).send("The course with given id was not found");
-            return;
-    }
-    
-    const index = courses.indexOf(course);
-    courses.splice(index, 1);
-    res.send(course);
+    const id = req.params.id;
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) return res.status(400).send("Not valid id format...");
+
+    db.removeCourse(id).then(function(result){
+        res.send(result);
+    }).catch(function(err){
+        let errorCode = err.cause.code;
+        res.status(errorCode).send(err.message);
+    });
 });
 
-function validateCourse(course){
+function validateCourse(course,crud){
     const schema = Joi.object({
-        name : Joi.string().min(3).required()
-    });
+        name : Joi.string().min(3).alter({
+            post: (schema) => schema.required(),
+            put: (schema) => schema.optional()
+        }),
+        author: Joi.string().min(3),
+        tags: Joi.array().items(Joi.string()),
+        date: Joi.date(),
+        isPublished: Joi.boolean()
+    }).tailor(crud);
 
     return schema.validate(course);
 }
